0e0d4f282fd403679e7b9ba64372c315031f6695
lillywolf <lillywolf@gmail.com>
2011-11-13 14:49:01 -0800

    added authentication

--- index.php
+++ index.php
@@ -2,24 +2,46 @@
 
 	require_once('php-sdk/src/facebook.php');
 
-	$config = array(
-  		'appId' => '107796503671',
-  		'secret' => '10cc0163136a373aa6192f6ceafda96e',
-	);
+	$appId = '107796503671';
+	$appSecret = '10cc0163136a373aa6192f6ceafda96e';
+	$appCanvas = 'thebandapp';
+	
+	$config = array();
+	$config[‘appId’] = $appId;
+	$config[‘secret’] = $appSecret;
+	$config[‘fileUpload’] = false; // optional	
 
 	$facebook = new Facebook($config);
+		
 	$user_id = $facebook->getUser();
-	if($user_id)
-	{
 	
+	if($user_id) {
+		try {
+			$user_profile = $facebook->api('/me', 'GET');			
+		}
+		catch(FacebookApiException $e) {
+			$loginUrl = $facebook->getLoginUrl($params);			
+	        error_log($e->getType());
+	        error_log($e->getMessage());			
+		}	
 	}
-	else
-	{
-		header('Location:' . $facebook->getLoginURL());
+	else {
+		$params = array(
+		  scope => 'email, publish_stream, manage_pages',
+		  redirect_uri => 'apps.facebook.com/' . $appCanvas
+		);
+		$loginUrl = $facebook->getLoginUrl($params);
+			
+		// Use this for non-facebook canvas page (i.e. Facebook Connect)		
+		// header('Location:' . $facebook->getLoginURL());
 	}
 
-	print_r($user_id);
 	$signed_request = $facebook->getSignedRequest();
-	print_r($signed_request['page']);
+	if ($signed_request['page']['liked'] {
+		// Show downloads and other content for users who liked the page
+	}
+	else {
+		// Show default page for users who haven't liked it
+	}
 
 ?>
