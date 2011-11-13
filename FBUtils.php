2a88738d4b460cf3202247b79b107b7bf7ebad75
lillywolf <lillywolf@gmail.com>
2011-11-12 17:32:07 -0800

    testing signed requests

--- FBUtils.php
+++ FBUtils.php
@@ -65,9 +65,9 @@ class FBUtils {
 
 	$facebook = new Facebook($config);	
 	print_r("requesting signed request");
-	print_r("fb: " . $facebook);
+	print_r("fb: " . $facebook, true);
 	$request = $facebook->getSignedRequest();
-	print_r("req: " . $request);
+	print_r("req: " . $request, true);
 	return $request;	
   }
 
