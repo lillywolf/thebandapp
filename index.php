869f0f45016aa658a32d81231aa2d75d6356acc8
lillywolf <lillywolf@gmail.com>
2011-11-12 18:32:28 -0800

    test

--- index.php
+++ index.php
@@ -1,40 +1,17 @@
 <?php
 
-try{
-    include_once "php-sdk/src/facebook.php";
-}
-catch(Exception $o){
-    echo '<pre>';
-    print_r($o);
-    echo '</pre>';
-}
+// Remember to copy files from the SDK's src/ directory to a
+// directory in your application on the server, such as php-sdk/
+require_once('php-sdk/src/facebook.php');
 
-  $app_id = '107796503671';
-  $app_secret = '10cc0163136a373aa6192f6ceafda96e';
+$config = array(
+  'appId' => '107796503671',
+  'secret' => '10cc0163136a373aa6192f6ceafda96e',
+);
 
-  print_r("test");
-  $config = array();
-  $config['appId'] = $app_id;
-  $config['secret'] = $app_secret;
+$facebook = new Facebook($config);
+$user_id = $facebook->getUser();
 
-  $facebook = new Facebook(array(
-	'appId'  => '107796503671',
-	'secret' => '10cc0163136a373aa6192f6ceafda96e',
-  ));
-
-	$session = $facebook->getSession();
-
-	$fbme = null;
-    // Session based graph API call.
-    if ($session) {
-      try {
-        $uid = $facebook->getUser();
-        $fbme = $facebook->api('/me');
-      } catch (FacebookApiException $e) {
-          d($e);
-      }
-    }
-
-	print_r($uid);
+print_r($user_id);
 
 ?>
