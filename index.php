24cb8883099e71285fc497b803006a0021e99805
lillywolf <lillywolf@gmail.com>
2011-11-12 22:42:24 -0800

    testing signed requests

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <?php
 
-print_r("work");
+	require_once('php-sdk/src/facebook.php');
 
-// Remember to copy files from the SDK's src/ directory to a
-// directory in your application on the server, such as php-sdk/
-require_once('php-sdk/src/facebook.php');
+	$config = array(
+  		'appId' => '107796503671',
+  		'secret' => '10cc0163136a373aa6192f6ceafda96e',
+	);
 
-$config = array(
-  'appId' => '107796503671',
-  'secret' => '10cc0163136a373aa6192f6ceafda96e',
-);
-
-$facebook = new Facebook($config);
-$user_id = $facebook->getUser();
-if($user_id)
-{
+	$facebook = new Facebook($config);
+	$user_id = $facebook->getUser();
+	if($user_id)
+	{
 	
-}
-else
-{
-header('Location:'.$facebook->getLoginURL());
-}
+	}
+	else
+	{
+		header('Location:' . $facebook->getLoginURL());
+	}
 
-print_r($user_id);
+	print_r($user_id);
+	$signed_request = $facebook->getSignedRequest();
+	print_r($signed_request);
 
 ?>
