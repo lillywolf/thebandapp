5456a36c90292639df841c040be9669d7085618b
lillywolf <lillywolf@gmail.com>
2011-11-13 22:39:36 -0800

    testing javascript

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-	<script type="text/javascript" src="site/swfobject.js"></script>;
-	<script type="text/javascript" src="site/FBJSBridge.js"></script>;
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>;
-	<script type="text/javascript" src="site/history/history.js"></script>;
+	<script type="text/javascript" src="site/swfobject.js"></script>
+	<script type="text/javascript" src="site/FBJSBridge.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+	<script type="text/javascript" src="site/history/history.js"></script>
 </head>	
 <body>
 <?php
@@ -31,26 +31,34 @@
 				echo "liked";
 				?>
 				
-				<script type="text/javascript">
-	            var swfVersionStr = "10.2.0";
-	            var xiSwfUrlStr = "site/playerProductInstall.swf";
-	            var flashvars = {};
-	            var params = {};
-	            params.quality = "high";
-	            params.bgcolor = "#ffffff";
-	            params.allowscriptaccess = "sameDomain";
-	            params.allowfullscreen = "true";
-	            var attributes = {};
-	            attributes.id = "Main";
-	            attributes.name = "Main";
-	            attributes.align = "middle";
-	            swfobject.embedSWF(
-	                 "site/Main.swf", "flashContent", 
-	                 "510", "100%", 
-	                 swfVersionStr, xiSwfUrlStr, 
-	                 flashvars, params, attributes);
-	            swfobject.createCSS("#flashContent", "display:block;text-align:left;");				
-				</script>
+		        <noscript>
+		            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="100%" height="100%" id="Main">
+		                <param name="movie" value="site/Main.swf" />
+		                <param name="quality" value="high" />
+		                <param name="bgcolor" value="#ffffff" />
+		                <param name="allowScriptAccess" value="sameDomain" />
+		                <param name="allowFullScreen" value="true" />
+		                <!--[if !IE]>-->
+		                <object type="application/x-shockwave-flash" data="site/Main.swf" width="100%" height="100%">
+		                    <param name="quality" value="high" />
+		                    <param name="bgcolor" value="#ffffff" />
+		                    <param name="allowScriptAccess" value="sameDomain" />
+		                    <param name="allowFullScreen" value="true" />
+		                <!--<![endif]-->
+		                <!--[if gte IE 6]>-->
+		                    <p> 
+		                        Either scripts and active content are not permitted to run or Adobe Flash Player version
+		                        10.2.0 or greater is not installed.
+		                    </p>
+		                <!--<![endif]-->
+		                    <a href="http://www.adobe.com/go/getflashplayer">
+		                        <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash Player" />
+		                    </a>
+		                <!--[if !IE]>-->
+		                </object>
+		                <!--<![endif]-->
+		            </object>
+		        </noscript>
 				
 				<?php
 			} 
