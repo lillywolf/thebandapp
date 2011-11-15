738420eb536b623592e39cb65aa10efe99d7c89f
lillywolf <lillywolf@gmail.com>
2011-11-14 22:34:52 -0800

    added listener for like button

--- index.php
+++ index.php
@@ -34,18 +34,21 @@
 
 	<script type="text/javascript">
 	    window.fbAsyncInit = function() {
-	      FB.init({
-	        appId: '<?php echo $facebook->getAppID() ?>', 
-	        cookie: true, 
-	        xfbml: true,
-	        oauth: true
-	      });
-	      FB.Event.subscribe('auth.login', function(response) {
-	        window.location.reload();
-	      });
-	      FB.Event.subscribe('auth.logout', function(response) {
-	        window.location.reload();
-	      });
+			FB.init({
+	        	appId: '<?php echo $facebook->getAppID() ?>', 
+	        	cookie: true, 
+	        	xfbml: true,
+	        	oauth: true
+	      	});
+	      	// FB.Event.subscribe('auth.login', function(response) {
+	      	// 	        	window.location.reload();
+	      	// });
+	      	// FB.Event.subscribe('auth.logout', function(response) {
+	      	// 	        	window.location.reload();
+	      	// });
+			FB.Event.subscribe('edge.create', function(response) {
+	        	window.location.reload();
+			});	
 	    };
 	    (function() {
 	      var e = document.createElement('script'); e.async = true;
