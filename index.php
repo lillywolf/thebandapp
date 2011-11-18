d96504698fdca8e01591b137db27ae005d559ef4
lillywolf <lillywolf@gmail.com>
2011-11-18 15:16:33 -0800

    testing like button placement

--- index.php
+++ index.php
@@ -88,11 +88,12 @@
 		function getOffset( el ) {
 		    var _x = 0;
 		    var _y = 0;
-		    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-		        _x += el.offsetLeft - el.scrollLeft;
-		        _y += el.offsetTop - el.scrollTop;
-		        el = el.parentNode;
-		    }
+			_y = el.offsetTop - el.scrollTop;
+		    // while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+		    //     _x += el.offsetLeft - el.scrollLeft;
+		    //     _y += el.offsetTop - el.scrollTop;
+		    //     el = el.parentNode;
+		    // }
 			return _y;
 		    // return { top: _y, left: _x };
 		}				
