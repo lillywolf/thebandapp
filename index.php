e49c2425951fc3bfea8d2993af00c74e99f040ad
lillywolf <lillywolf@gmail.com>
2011-11-22 17:58:11 -0800

    height resize

--- index.php
+++ index.php
@@ -243,6 +243,7 @@
 					alert("not a page like!");
 				}
 			});	
+			FB.Canvas.setSize({ width: 520, height: 900 });
 	    };
 	    (function() {
 	      var e = document.createElement('script'); e.async = true;
