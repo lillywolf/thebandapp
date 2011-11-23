f5efd66451248d654ecb8689c571e4f22dac7aaa
lillywolf <lillywolf@gmail.com>
2011-11-22 23:48:07 -0800

    connecting to db

--- index.php
+++ index.php
@@ -100,6 +100,33 @@
 
 	$facebook = new Facebook($config);	
 	$user_id = $facebook->getUser();
+	
+	$user="uc3rwdprf7ijm9z";
+	$pass="pab1kv3jcunuilewgh4op7kwht";
+	$host="ec2-107-22-196-151.compute-1.amazonaws.com";
+	$dbname="dcw8wyqwdih0rv";
+	# This function reads your DATABASE_URL config var and returns a connection
+	# string suitable for pg_connect. Put this in your app.
+	function pg_connection_string_from_database_url() {
+	  extract(parse_url($_ENV["DATABASE_URL"]));
+	  return "user=$user password=$pass host=$host dbname=$dbname sslmode=require"; # <- you may want to add sslmode=require there too
+	}
+
+	# Here we establish the connection. Yes, that's all.
+	$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+	# Now let's use the connection for something silly just to prove it works:
+	$result = pg_query($pg_conn, "SELECT venue FROM shows WHERE artist_id=1");
+
+	print "<pre>\n";
+	if (!pg_num_rows($result)) {
+	  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+	} else {
+	  print "Tables in your database:\n";
+	  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+	}
+	print "\n";	
+	
 	?>
 	
     <div id="fb-root"></div>
