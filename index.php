017d0c34b3e785fc24f8452bb2a76d1ea7d8ca8d
lillywolf <lillywolf@gmail.com>
2011-11-23 23:41:56 -0800

    testing swf load

--- index.php
+++ index.php
@@ -75,18 +75,18 @@
 		}
 
 		# Here we establish the connection
-		// $pg_conn = pg_connect(pg_connection_string_from_database_url());
-		// # Get shows data
-		// $result = pg_query($pg_conn, "SELECT venue FROM shows WHERE artist_id=1");
-		// # Print shows data
-		// print "<div id='shows'><img id='shows-header' src='/images/headers/shows_header.png'/>";
-		// if (!pg_num_rows($result)) {
-		// } else {
-		//   	while ($row = pg_fetch_row($result)) { 
-		// 		print("<span class='show'>$row[0]</span>"); 
-		// 	}
-		// }
-		// print "</div>";	
+		$pg_conn = pg_connect(pg_connection_string_from_database_url());
+		# Get shows data
+		$result = pg_query($pg_conn, "SELECT venue FROM shows WHERE artist_id=1");
+		# Print shows data
+		print "<div id='shows'><img id='shows-header' src='/images/headers/shows_header.png'/>";
+		if (!pg_num_rows($result)) {
+		} else {
+	   		while ($row = pg_fetch_row($result)) { 
+		 		print("<span class='show'>$row[0]</span>"); 
+		 	}
+		}
+		print "</div>";	
 	
 		?>
 	
