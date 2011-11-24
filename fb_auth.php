6aa51b71c3688273a9b426e3b66223d6b84f35d1
lillywolf <lillywolf@gmail.com>
2011-11-24 00:45:31 -0800

    moving fb php to fb_auth

--- fb_auth.php
+++ fb_auth.php
@@ -1,46 +1,54 @@
-<?php
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#">
 
-	require_once('php-sdk/src/facebook.php');
+<body>
+	found fb_auth
 
-	$appId = '107796503671';
-	$appSecret = '10cc0163136a373aa6192f6ceafda96e';
-	$appUrl = 'http://apps.facebook.com/thebandapp';	
-	$fbPageUrl = "facebook.com/lillywolfmusic";
+	<?php
 
-	$config = array();
-	$config['appId'] = $appId;
-	$config['secret'] = $appSecret;
-	$config['fileUpload'] = false; // optional	
+		require_once('php-sdk/src/facebook.php');
 
-	$facebook = new Facebook($config);	
-	$user_id = $facebook->getUser();
-	$this->getSignedRequest();
+		$appId = '107796503671';
+		$appSecret = '10cc0163136a373aa6192f6ceafda96e';
+		$appUrl = 'http://apps.facebook.com/thebandapp';	
+		$fbPageUrl = "facebook.com/lillywolfmusic";
+
+		$config = array();
+		$config['appId'] = $appId;
+		$config['secret'] = $appSecret;
+		$config['fileUpload'] = false; // optional	
+
+		$facebook = new Facebook($config);	
+		$user_id = $facebook->getUser();
+		$this->getSignedRequest();
 	
-	function getSignedRequest() {
-		if($user_id) {
-			try {
-				$user_profile = $facebook->api('/me', 'GET');	
+		function getSignedRequest() {
+			if($user_id) {
+				try {
+					$user_profile = $facebook->api('/me', 'GET');	
+					$signed_request = $facebook->getSignedRequest();
+					if ($signed_request['page']['liked']) { 
+						echo '<script>alert("got req");</script>';
+					} else {
+						echo '<script>alert("got req");</script>';
+					}					
+				}
+				catch(FacebookApiException $e) {
+					$loginUrl = $facebook->getLoginUrl($params);			
+			        error_log($e->getType());
+			        error_log($e->getMessage());			
+				}	
+			}
+			else {		
 				$signed_request = $facebook->getSignedRequest();
 				if ($signed_request['page']['liked']) { 
 					echo '<script>alert("got req");</script>';
 				} else {
 					echo '<script>alert("got req");</script>';
-				}					
-			}
-			catch(FacebookApiException $e) {
-				$loginUrl = $facebook->getLoginUrl($params);			
-		        error_log($e->getType());
-		        error_log($e->getMessage());			
-			}	
-		}
-		else {		
-			$signed_request = $facebook->getSignedRequest();
-			if ($signed_request['page']['liked']) { 
-				echo '<script>alert("got req");</script>';
-			} else {
-				echo '<script>alert("got req");</script>';
-			}	
-		}		
-	}	
+				}	
+			}		
+		}	
 
-?>
+	?>
+	
+	</body>
+</html>	
