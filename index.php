7982923e7139f960b26c4549eb1f8999f067e1dd
lillywolf <lillywolf@gmail.com>
2011-11-24 14:51:59 -0800

    testing JS SDK

--- index.php
+++ index.php
@@ -273,12 +273,12 @@
 		};
 
 		// Load the SDK Asynchronously
-		(function(d){
-			var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-		    js = d.createElement('script'); js.id = id; js.async = true;
-		    js.src = "//connect.facebook.net/en_US/all.js";
-		    d.getElementsByTagName('head')[0].appendChild(js);
-		}(document));
+	 	(function() {
+			var e = document.createElement('script'); e.async = true;
+		    e.src = document.location.protocol +
+		    '//connect.facebook.net/en_US/all.js';
+			document.getElementById('fb-root').appendChild(e);
+		}());
 	
 	</script>
 	
