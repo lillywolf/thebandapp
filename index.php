c80de60a6a86730baaa7beb455d13cf48087a506
lillywolf <lillywolf@gmail.com>
2011-11-25 18:04:37 -0800

    getting page posts

--- index.php
+++ index.php
@@ -39,16 +39,7 @@
 			$downloads_enabled = "false";
 		}
 
-		$after = microtime();
-		
-		# FOR ADMIN PANEL
-
-		$state = md5(uniqid(rand(), TRUE));
-		$scope = 'email,publish_stream,read_stream,manage_pages';
-		$home = getHome();
-		$authorize_url = "https://www.facebook.com/dialog/oauth?client_id=$appId" .
-		      	"&redirect_uri=$home&state=" . $state . "&scope=$scope";		
-		      	echo("<script> top.location.href='" . $authorize_url . "'</script>");		
+		$after = microtime();	
 				
 		?>
 		
@@ -290,6 +281,16 @@
 	function getHome () {
 	  return ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?: "http") . "://" . $_SERVER['HTTP_HOST'] . "/";
 	}
+	
+		
+	# FOR ADMIN PANEL
+
+	/* $state = md5(uniqid(rand(), TRUE));
+	$scope = 'email,publish_stream,read_stream,manage_pages';
+	$home = getHome();
+	$authorize_url = "https://www.facebook.com/dialog/oauth?client_id=$appId" .
+	      	"&redirect_uri=$home&state=" . $state . "&scope=$scope";		
+	      	echo("<script> top.location.href='" . $authorize_url . "'</script>"); */	
 
 	# Use this for non-facebook canvas page (i.e. Facebook Connect)		
 	# header('Location:' . $facebook->getLoginURL());	
