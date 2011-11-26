083c8cf0862f298ecbf1a9f87b4da1814cca5ebf
lillywolf <lillywolf@gmail.com>
2011-11-25 17:59:33 -0800

    getting page posts

--- index.php
+++ index.php
@@ -258,7 +258,7 @@
 						e.appendChild(pd);						
 						document.getElementById('extra-content').appendChild(e);
 					}
-				  // alert('Your p: ' + response);
+				  alert('Your p: ' + response.length);
 				});		
 			};
 
