5ada0836bf8e894634d2a2f1ae98762942ad12bb
lillywolf <lillywolf@gmail.com>
2011-12-05 22:43:46 -0800

    fixed JS path

--- html5/index.php
+++ html5/index.php
@@ -2,7 +2,7 @@
 <head>
 	<!--script type="text/javascript" src="site/swfobject.js"></script-->
 	<!--script type="text/javascript" src="site/FBJSBridge.js"></script-->
-	<script type="text/javascript" src="scripts/spin.js"></script>	
+	<script type="text/javascript" src="../scripts/spin.js"></script>	
 	<!--script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script-->	
 	<!--script type="text/javascript" src="scripts/prototype.js"></script-->
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
