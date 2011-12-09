0b94e5c13b47aa0d208063577958c828adf84a32
lillywolf <lillywolf@gmail.com>
2011-12-08 18:16:02 -0800

    styling html5

--- html5/index.php
+++ html5/index.php
@@ -102,6 +102,8 @@
 						<?php $i = 1; foreach ($trackdata as $track) {
 							echo '<div class="song" onClick="populatePlayer(\'' . $track['title'] . '\', ' . $i . '); document.getElementById(\'audio_' . $i . '\').play()"><audio class="audio_tag" id="audio_' . $i . '" controls><source src="' . $track['stream_url'] . '?secret_token=1-12872-7625335-94e91695a1ea1e98&client_id=738091d6d02582ddd19de7109b79e47b" type="audio/mpeg" /></audio>
 							<div class="song_title">' . $track['title'] . '</div>
+							<div class="download_btn_wrapper"><div class="download_btn"></div></div>
+							<div class="buy_btn_wrapper"><div class="buy_btn"></div></div>
 							<!--button onClick="document.getElementById(\'audio_' . $i . '\').pause()">Pause</button--></div>';
 							$i++;
 						} ?>
