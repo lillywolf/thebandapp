fca7d05963ddd5a63cc441ff964c26da8378d268
lillywolf <lillywolf@gmail.com>
2011-12-12 15:26:39 -0800

    update to player code

--- html5/index.php
+++ html5/index.php
@@ -179,20 +179,20 @@
 		// var timeleft = $('#top_player #top_timer');
 		// var topPlayer = $('#top_player');
 		
-		function addAudioListeners() {
-			var audio = document.getElementById('top_audio');
-			if ((audio.buffered != undefined) && (audio.buffered.length != 0)) {
-				$(audio).bind('progress', function) {
-					var loaded = parseInt(((audio.buffered.end(0) / audio.duration) * 100), 10);
-				}
-				$(audio).bind('timeupdate', function() {
-					var rem = parseInt(audio.duration - audio.currentTime, 10),
-				  	pos = (audio.currentTime / audio.duration) * 100,
-				  	mins = Math.floor(rem/60,10),
-				  	secs = rem - mins*60;
-				timeleft.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
-			}
-		}
+		// function addAudioListeners() {
+		// 	var audio = document.getElementById('top_audio');
+		// 	if ((audio.buffered != undefined) && (audio.buffered.length != 0)) {
+		// 		$(audio).bind('progress', function) {
+		// 			var loaded = parseInt(((audio.buffered.end(0) / audio.duration) * 100), 10);
+		// 		}
+		// 		$(audio).bind('timeupdate', function() {
+		// 			var rem = parseInt(audio.duration - audio.currentTime, 10),
+		// 		  	pos = (audio.currentTime / audio.duration) * 100,
+		// 		  	mins = Math.floor(rem/60,10),
+		// 		  	secs = rem - mins*60;
+		// 		timeleft.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
+		// 	}
+		// }
 		
 		function swapAudio(url) {
 			alert(url);
