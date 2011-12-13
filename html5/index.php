3829ca970c0ebdb84aeeeedaac183df3222c6a9e
lillywolf <lillywolf@gmail.com>
2011-12-12 16:59:02 -0800

    adding player code

--- html5/index.php
+++ html5/index.php
@@ -191,6 +191,7 @@
 		 				pos = (audio.currentTime / audio.duration) * 100,
 		 		  		mins = Math.floor(rem/60, 10),
 		 		  		secs = rem - mins*60;
+					alert(secs);
 		 			timeleft.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
 				});
 		 	}
