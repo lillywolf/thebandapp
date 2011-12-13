5d37047c51aec36d9c0fb32c86e898c21d899185
lillywolf <lillywolf@gmail.com>
2011-12-12 16:47:00 -0800

    adding player code

--- html5/index.php
+++ html5/index.php
@@ -188,12 +188,12 @@
 		 		$(audio).bind('progress', function) {
 		// 			var loaded = parseInt(((audio.buffered.end(0) / audio.duration) * 100), 10);
 		 		});
-		 		$(audio).bind('timeupdate', function() {
-		 			var rem = parseInt(audio.duration - audio.currentTime, 10),
-		 			pos = (audio.currentTime / audio.duration) * 100,
-		 		  	mins = Math.floor(rem/60,10),
-		 		  	secs = rem - mins*60;
-		 		timeleft.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
+		 		$(audio).bind('timeupdate', function) {
+		 		// 	var rem = parseInt(audio.duration - audio.currentTime, 10),
+		 		// 	pos = (audio.currentTime / audio.duration) * 100,
+		 		//   	mins = Math.floor(rem/60,10),
+		 		//   	secs = rem - mins*60;
+		 		// timeleft.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
 				});
 		 	}
 		}
