0e7ee2658691e5b07eb2955d9b94c1bcf65ef7d4
lillywolf <lillywolf@gmail.com>
2011-12-18 23:40:32 -0800

    monkeying with soundmanager

--- html5/index.php
+++ html5/index.php
@@ -13,6 +13,7 @@
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
 	<script src="http://connect.soundcloud.com/sdk.js" type="text/javascript"></script>	
 	<script src="../scripts/modernizr.custom.41971.js" type="text/javascript"></script>	
+	<script src="../scripts/sm2/sm2_init.js" type="text/javascript"></script>	
 	<!--script type="text/javascript" src="site/history/history.js"></script-->
 </head>	
 <body>
