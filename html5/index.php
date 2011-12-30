a2d7bd238a84c1d8208e23cfb8f5035d3610699a
lillywolf <lillywolf@gmail.com>
2011-12-30 00:26:06 -0500

    fixing page forwarding

--- html5/index.php
+++ html5/index.php
@@ -201,10 +201,6 @@
 		<img src="../images/html5/song_bg_hover2.png" style="display: none" />
 		
 	<script type="text/javascript">
-	
-		if (top.location.href != 'http://www.facebook.com/lillywolfanddrnu?sk=app_107796503671') {
-			top.location.href = 'http://www.facebook.com/lillywolfanddrnu?sk=app_107796503671';
-		}
 		
 		var MAX_TRACKS = 4;
 		var MAX_POSTS = 5;
