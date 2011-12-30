b86a33c13dbf59181b1021d4a11087dbd95dbd66
lillywolf <lillywolf@gmail.com>
2011-12-30 00:07:21 -0500

    fixing errors

--- html5/index.php
+++ html5/index.php
@@ -796,9 +796,9 @@
 		// },'html');	
 		
 		// CREATE MISSION
-		// $.get('../redis/page_interaction.php?fbId=<?php echo $user_id ?>&pageId=<?php echo $pageId ?>&method=create_mission&mission_id=like', function(data, status) {
-		//       // parse
-		// },'html');
+		$.get('../redis/page_interaction.php?fbId=<?php echo $user_id ?>&pageId=<?php echo $pageId ?>&method=create_mission&mission_id=like', function(data, status) {
+		      // parse
+		},'html');
 	
 	</script>
 	
