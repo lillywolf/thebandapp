201b251b07302451f7202c308b8e24605c5d14e6
lillywolf <lillywolf@gmail.com>
2012-01-01 20:42:14 -0500

    updating song hover buttons

--- html5/index.php
+++ html5/index.php
@@ -400,7 +400,7 @@
 				var titleElem = $(str)[0];
 				var titleIdStr = '#' + titleElem.id.toString();
 				// elem.style.backgroundImage = "url('../images/html5/song_bg_hover.png')";
-				var titleOffsetTop = parseInt(titleElem.offsetTop) - parseInt(document.getElementById('player').offsetTop) - 80;
+				var titleOffsetTop = parseInt(titleElem.offsetTop) - parseInt(document.getElementById('player').offsetTop) - 20;
 				var titleOffsetLeft = parseInt(titleElem.offsetLeft) + parseInt($(titleIdStr).width()) - 10;
 				document.getElementById('song_play_btn_over').style.left = titleOffsetLeft;
 				document.getElementById('song_play_btn_over').style.top = titleOffsetTop;
