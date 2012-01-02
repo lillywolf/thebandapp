ac18f681479965c81c38807cd4ff046f17521b02
lillywolf <lillywolf@gmail.com>
2012-01-01 21:29:20 -0500

    updating song hover buttons

--- html5/index.php
+++ html5/index.php
@@ -391,7 +391,7 @@
 			$('.buy_song').bind('click', function(event) {
 				event.stopPropagation();	
 			});
-			$('#song_play_btn_over').unbind('mouseover');
+			$('#song_play_btn_over').unbind('mouseover mousemove mouseleave');
 		}
 		
 		function listenForHovers() {
