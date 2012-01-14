b4b7db508e84635fd3630ebb1e9d7dae0f103e4c
lillywolf <lillywolf@gmail.com>
2012-01-13 19:11:19 -0800

    fixed play logging

--- html5/index.php
+++ html5/index.php
@@ -522,8 +522,8 @@
 		
 		function swapAudio(url, trackIndex) {
 			// Log plays
-			$.get('../redis/page_interaction.php?fbId=<?php echo $user_id ?>&pageId=<?php echo $pageId ?>&method=log_click&clickType='+currentSongData.title, function(data, status) {
-			});
+			// $.get('../redis/page_interaction.php?fbId=<?php echo $user_id ?>&pageId=<?php echo $pageId ?>&method=log_click&clickType='+currentSongData.title, function(data, status) {
+			// });
 			
 			var idStr = 'audio_'+trackIndex.toString();
 			var topAudio = document.getElementById(idStr);
