0ba681658b20dc56df0c2def7b3fea1aaa2254f3
lillywolf <lillywolf@gmail.com>
2012-01-14 22:38:21 -0800

    added script to get sc users

--- html5/getusers.php
+++ html5/getusers.php
@@ -1,5 +1,8 @@
 <?php
 
+	require_once('../sc-api/Soundcloud.php');
+
+
 	$soundcloud = new Services_Soundcloud('738091d6d02582ddd19de7109b79e47b', 'b8f231ac6dc380b6efb2a8a88cd6d9fe', 'http://simple-ocean-7178.herokuapp.com/auth/');
 	$soundcloud->setAccessToken('1-12872-7625335-e561f85b896d9158');
 	
