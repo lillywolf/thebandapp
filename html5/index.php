f17cba1bc30d544ace3c7af4f4239bfb71b0627d
lillywolf <lillywolf@gmail.com>
2012-01-20 16:36:54 -0800

    logging set ids

--- html5/index.php
+++ html5/index.php
@@ -77,6 +77,7 @@
 		$playlist_id = null;
 		$playlists = null;
 		foreach ($playlistdata as $playlist) {
+			error_log('playlist id: ' . $playlist['id']);
 			$download_tracks_urls = '';
 			$download_tracks = $playlist['tracks']; 
 			foreach($download_tracks as $track) {
