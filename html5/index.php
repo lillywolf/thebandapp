fb0db22ffe2838d3b5b188062bd78859d60aaa00
lillywolf <lillywolf@gmail.com>
2012-01-21 19:48:40 -0800

    added banner fades

--- html5/index.php
+++ html5/index.php
@@ -315,14 +315,14 @@
 		}
 		
 		function swapBanner() {
-			$('#listen_img').fadeOut(550, 'linear');
+			$('#listen_img').fadeOut(1000, 'linear');
 			var e = window.document.createElement('img');
 			e.src = '../images/banners/like_song_light.jpg';
 			e.id = 'like_img';
 			document.getElementById('listen_banner').appendChild(e);
 			$('#like_img').hide();		
-			$('#like_img').fadeIn(550, 'linear');
-			$('#banner_song_like').html('<fb:like href="' + currentSongData.url + '" layout="standard" show_faces="false" action="like" font="arial" colorscheme="light" send="false" width="225" />');
+			$('#like_img').fadeIn(1000, 'linear');
+			$('#banner_song_like').html('<fb:like href="' + currentSongData.url + '" layout="box_count" show_faces="false" action="like" font="arial" colorscheme="light" send="false" />');
 			if (typeof FB !== 'undefined') {
 			    FB.XFBML.parse(document.getElementById('banner_song_like'));
 			}					
